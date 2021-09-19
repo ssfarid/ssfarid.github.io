c6b61b50608af59b5a928fdf50615f8fd67a0546
ssfarid <90166558+ssfarid@users.noreply.github.com>
2021-09-19 22:01:32 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,8 +107,13 @@ let swiper = new Swiper(".portfolio__container", {
     },
 });
 
-/*==================== TESTIMONIAL ====================*/
-
+/*==================== SCROLL REVEAL ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
@@ -178,13 +183,39 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
 
-
-
-
-
-//about
-
+/*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__container',{delay: 400}); 
-sr.reveal('.about__data',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL EDUCATION*/
+sr.reveal('.education__content',{interval:200}); 
+
+/*SCROLL EXPERIENCE*/
+sr.reveal('.experience__content',{interval:200});
+
+
+
+
+/*SCROLL WORK*/
+sr.reveal('.work',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
+
+
+
+//footer button
+
